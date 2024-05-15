cf5eb5c16c8c78f25af8acec657b25ca781534d9
hyun <starringbell@gmail.com>
2024-05-15 20:15:17 +0900

    project 초기 설정

---
+++ src/main/java/com/example/keephamapi/KeephamApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.keephamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class KeephamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeephamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keephamapi/KeephamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keephamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeephamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
