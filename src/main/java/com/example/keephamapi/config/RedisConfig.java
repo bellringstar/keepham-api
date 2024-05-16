65e81720f8d95c7572548c7cc94051c8963dcbc6
hyun <starringbell@gmail.com>
2024-05-16 15:44:25 +0900

    feat: redis 설정 추가

---
+++ src/main/java/com/example/keephamapi/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.example.keephamapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@PropertySource("classpath:application-redis.yml")
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisConfig {
+
+    @Value("${host}")
+    private String host;
+    @Value("${port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+}
