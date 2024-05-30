17d8925f6b4d23f93265cc64968abf36e142997a
hyun <starringbell@gmail.com>
2024-05-30 19:45:06 +0900

    feat: 웹소켓 연결 핸드셰이크시 헤더에서 토큰정보 꺼내 검증

---
+++ keepham-chat/src/main/java/com/example/keephamchat/config/websocket/JwtHandshakeInterceptor.java
@@ -0,0 +1,68 @@
+package com.example.keephamchat.config.websocket;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtHandshakeInterceptor implements HandshakeInterceptor {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+                                   Map<String, Object> attributes) throws Exception {
+        if (request instanceof ServletServerHttpRequest) {
+            HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+            String token = extractToken(servletRequest);
+
+            if (token != null) {
+                return validateToken(token, attributes, response);
+            }
+
+            log.warn("Missing Token");
+            response.setStatusCode(HttpStatus.FORBIDDEN);
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+                               Exception exception) {
+
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String header = request.getHeader(AUTHORIZATION_HEADER);
+        if (header != null && header.startsWith(BEARER_PREFIX)) {
+            return header.substring(BEARER_PREFIX.length());
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token, Map<String, Object> attributes, ServerHttpResponse response) {
+        String userId = redisTemplate.opsForValue().get(token);
+        if (userId != null) {
+            attributes.put("userId", userId);
+            return true;
+        }
+        log.warn("Invalid token or token expired");
+        response.setStatusCode(HttpStatus.FORBIDDEN);
+        return false;
+    }
+}
