8ee1a8bad3ca7aea28337a0dac73883c57b40528
hyun <starringbell@gmail.com>
2024-05-30 19:07:38 +0900

    refact: chat서버 분리로 인한 폴더구조변경

---
+++ keepham-chat/src/main/java/com/example/keephamchat/KeephamChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.keephamchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeephamChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeephamChatApplication.class, args);
+	}
+
+}
---
+++ keepham-chat/src/test/java/com/example/keephamchat/KeephamChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keephamchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeephamChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
